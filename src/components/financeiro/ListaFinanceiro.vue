5fa8e0dd4e45b350974b2e9bdc8bfbc0a2908ca9
LuizGabriel2000 <luizgabriel.lg2000@outlook.com>
2022-07-25 10:39:36 -0300

    Desenvolvida o layout do projeto!

---
+++ src/components/financeiro/ListaFinanceiro.vue
@@ -0,0 +1,82 @@
+<template>
+    <v-card color="rgb(0 0 0 / 9%)" class="px-4 py-2">
+        <h2 class="branco">{{dados.titulo}}</h2>
+        <v-simple-table
+            dark
+            fixed-header
+            height="450px"
+        >
+            <template v-slot:default>
+            <thead>
+                <tr>
+                <th class="text-left">
+                    Name
+                </th>
+                <th class="text-left">
+                    Valor
+                </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr
+                v-for="item in desserts"
+                :key="item.Nome"
+                >
+                <td>{{ item.Nome }}</td>
+                <td>{{ item.Valor }}</td>
+                </tr>
+            </tbody>
+            </template>
+        </v-simple-table>
+        <v-divider></v-divider>
+        <div class="d-flex mt-2">
+            <strong class="branco">Total:</strong>
+            <v-spacer></v-spacer>
+            <strong class="branco">R${{dados.total}}</strong>
+        </div>
+    </v-card>
+</template>
+
+<script>
+export default {
+    props: {
+        dados: Object
+    },
+    data () {
+      return {
+        desserts: [
+          {
+            Nome: 'Trabalho Frelancer',
+            Valor: 1900,
+          },
+          {
+            Nome: 'pagamento',
+            Valor: 237,
+          },
+          {
+            Nome: 'venda livros',
+            Valor: 262,
+          },
+          {
+            Nome: 'venda',
+            Valor: 305,
+          },
+          {
+            Nome: 'Gingerbread',
+            Valor: 356,
+          },
+          {
+            Nome: 'Jelly bean',
+            Valor: 375,
+          }
+        ],
+      }
+    }
+}
+</script>
+
+<style>
+.branco {
+    color: #fff;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-container>
+    <v-row>
+        <v-col class="d-flex" cols="12">
+            <h1 class="branco ml-2">Extrato financeiro</h1>
+            <v-spacer></v-spacer>
+            <v-btn class="mx-2" outlined color="primary" depressed @click="showCadastro(true)">Cadastrar receita</v-btn>
+            <v-btn class="mx-2" color="primary" depressed @click="showCadastro(true)">Cadastrar despesa</v-btn>
+        </v-col>
+
+        <v-col class="px-5" cols="12" sm="12" md="6">
+            <ListaFinanceiro :dados="receita"/>
+        </v-col>
+
+        <v-col class="px-5" cols="12" sm="12" md="6">
+            <ListaFinanceiro :dados="despesa"/>
+        </v-col>
+    </v-row>
+    <CadastroModal :show="dialog" :send="() => showCadastro(false)"/>
+  </v-container>
+</template>
+
+<script>
+import CadastroModal from '@/components/financeiro/CadastroModal.vue'
+import ListaFinanceiro from '../components/financeiro/ListaFinanceiro.vue'
+export default {
+    components: { CadastroModal, ListaFinanceiro },
+    data() {
+        return {
+            dialog: false,
+            receita: {
+                titulo: "Receita",
+                total: 23430.03
+            },
+            despesa: {
+                titulo: "Despesa",
+                total: 1000.03
+            }
+        };
+    },
+    methods: {
+        showCadastro(bool) {
+            this.dialog = bool
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
